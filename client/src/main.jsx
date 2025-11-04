0c2b2fd602c4744506003f96b30507acdb96e8c6
Gregory Bowne <gbowne144@gmail.com>
2025-11-03 22:20:11 -0800

    Initialize main.jsx with React app structure

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import store from './store.js';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme(); // Customize if needed
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
