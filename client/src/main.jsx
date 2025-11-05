d390f94a075483c80be369d7d7c1402502a8a7f4
Gregory Bowne <gbowne144@gmail.com>
2025-11-05 00:36:41 -0800

    Update main.jsx
    Import missing stylesheet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import store from './store.js';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import './index.css';
 
 const theme = createTheme(); // Customize if needed
 
